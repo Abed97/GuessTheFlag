cf380eec91cafb6dfe45ad062ee29a989643e1a8
Abed Atassi <abed.s.atassi@gmail.com>
2021-09-09 20:23:13 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Abed Atassi on 2021-09-09.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
